4da86ca224c55dd6aeb9a5fa7f3ccfc52abf88ae
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-04-03 19:15:05 -0300

    [add] cont and deposit

---
+++ test.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+contas = {
+    "1": {
+        "saldo": 10.0
+    },
+    "2": {
+        "saldo": 20.0
+    }
+}
+
+class Conta(Resource):
+    def get(self, conta_id):
+        if conta_id not in contas:
+            return "Conta não encontrada", 404
+        else:
+            return contas[conta_id], 200
+
+    def post(self, conta_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("valor")
+        args = parser.parse_args()
+
+        if conta_id not in contas:
+            return "Conta não encontrada", 404
+        else:
+            contas[conta_id]["saldo"] += float(args["valor"])
+            return contas[conta_id], 201
+
+api.add_resource(Conta, '/<string:conta_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
