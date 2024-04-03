3d9a8b56e895600e6e57bdf75c21173695d78ea9
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-04-03 19:35:21 -0300

    [add] deposit in URL

--- test.py
+++ test.py
@@ -21,19 +21,15 @@ class getValorConta(Resource):
             return contas[conta_id], 200
 
 class Deposito(Resource):
-    def post(self, conta_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("valor")
-        args = parser.parse_args()
-
+    def post(self, conta_id, value=None):
         if conta_id not in contas:
             return "Conta não encontrada", 404
         else:
-            contas[conta_id]["saldo"] += float(args["valor"])
+            contas[conta_id]["saldo"] += value
             return contas[conta_id], 201
 
-api.add_resource(getValorConta, '/conta/<string:conta_id>')
-api.add_resource(Deposito, '/conta/deposito/<string:conta_id>')
+api.add_resource(getValorConta, '/<string:conta_id>')
+api.add_resource(Deposito, '/<string:conta_id>/<float:value>')
 
 if __name__ == '__main__':
     app.run(debug=True)
